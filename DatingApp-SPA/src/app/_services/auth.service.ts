b4803bc0534849db40e98d6631b793d0c2c813a6
Knudsen <gknudsen@duqlight.com>
2018-12-10 14:31:36 -0500

    api error handling

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
 
-constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) { }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model)
