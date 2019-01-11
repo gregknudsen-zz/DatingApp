9f9b8917a79480c0004d5858c57ed6761b475dbe
Knudsen <gknudsen@duqlight.com>
2019-01-11 13:59:58 -0500

    error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged In!');
     }, error => {
-      console.log('Login Failed!');
+      console.log(error);
     });
   }
 
