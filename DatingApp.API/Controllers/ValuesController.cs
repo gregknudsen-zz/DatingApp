8ead782ed4a2a4f954089208598e9d1f1b64ae75
Knudsen <gknudsen@duqlight.com>
2019-01-31 10:28:17 -0500

    test role authorization

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
         // GET api/values
-        [AllowAnonymous]
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -29,7 +29,7 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
-        [AllowAnonymous]
+        [Authorize(Roles = "Member")]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
