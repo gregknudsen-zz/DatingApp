17690fb354cde8a011d7a489b6cc9bf26cba8053
Knudsen <gknudsen@duqlight.com>
2019-01-31 10:50:24 -0500

    add role name to user roles

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,17 +1,40 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class AdminController : ControllerBase
-    {   
+    {
+        private readonly DataContext _context;
+        public AdminController(DataContext context)
+        {
+            _context = context;
+
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("usersWithRoles")]
-        public IActionResult GetUsersWithRoles()
+        public async  Task<IActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins see this");
+            var userList = await (from user in _context.Users orderby user.UserName
+                                    select new 
+                                    {
+                                        Id = user.Id,
+                                        UserName = user.UserName,
+                                        Roles = (from userRole in user.UserRoles
+                                                    join role in _context.Roles
+                                                    on userRole.RoleId
+                                                    equals role.Id
+                                                    select role.Name).ToList()
+                                    }).ToListAsync();
+
+            return Ok(userList);
         }
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photosForModerators")]
